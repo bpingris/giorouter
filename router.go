aaea5a1bcb475903d31a17deb1649c14a90835cd
Benoit Pingris <benoit.pingris@pm.me>
2021-02-15 18:09:48 +0100

    feat: material.Theme is public, add comments, Layout does not pass material.Theme as a second arg

--- router.go
+++ router.go
@@ -5,38 +5,47 @@ import (
 	"gioui.org/widget/material"
 )
 
+// Route
 type Route interface {
-	Layout(gtx layout.Context, th *material.Theme)
+	Layout(gtx layout.Context) layout.Dimensions
 }
 
+// Routes
 type Routes map[string]Route
 
+// Router holds the precious information of the router
 type Router struct {
 	C      chan byte
 	Routes Routes
 	Stack  []string
-	th     *material.Theme
+	Th     *material.Theme
 }
 
-func NewRouter(th *material.Theme) *Router {
-	return &Router{
+// NewRouter returns an instance of a Router
+func NewRouter(th *material.Theme) Router {
+	return Router{
 		C:      make(chan byte, 1),
 		Routes: make(Routes),
-		th:     th,
+		Th:     th,
 	}
 }
 
+// SetRoutes links the provided paths to the Route
 func (r *Router) SetRoutes(routes Routes, initialRoute string) {
 	r.Routes = routes
 	r.Push(initialRoute)
 }
 
+// Push add a route to the stack and ask for a redraw
 func (r *Router) Push(route string) {
-	r.Stack = append(r.Stack, route)
-	r.Redraw()
+	if _, ok := r.Routes[route]; ok {
+		r.Stack = append(r.Stack, route)
+		r.Redraw()
+	}
 }
 
-func (r *Router) Top() string {
+// Top returns the current route
+func (r Router) Top() string {
 	l := len(r.Stack)
 	if l == 0 {
 		return ""
@@ -44,6 +53,7 @@ func (r *Router) Top() string {
 	return r.Stack[l-1]
 }
 
+// Pop pops a route out of the Routes stack and ask for a redraw (basically go back)
 func (r *Router) Pop() string {
 	if !r.CanPop() {
 		return ""
@@ -55,14 +65,17 @@ func (r *Router) Pop() string {
 	return top
 }
 
+// Redraw send an event through the Router'c Channel to ask for an immediate redraw
 func (r *Router) Redraw() {
 	r.C <- 0
 }
 
+// CanPop verifies if a previous route is available
 func (r *Router) CanPop() bool {
 	return len(r.Stack) > 1
 }
 
-func (r *Router) Layout(gtx layout.Context) {
-	r.Routes[r.Top()].Layout(gtx, r.th)
+// Layout draw the current route
+func (r *Router) Layout(gtx layout.Context) layout.Dimensions {
+	return r.Routes[r.Top()].Layout(gtx)
 }
